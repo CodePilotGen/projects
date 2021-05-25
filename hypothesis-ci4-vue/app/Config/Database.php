20c212a9e3eb5a2a0c401610667883168ff799df
leoeagle12 <84112353+leoeagle@users.noreply.github.com>
2021-05-24 18:17:59 -0700

    db name change

--- hypothesis-ci4-vue/app/Config/Database.php
+++ hypothesis-ci4-vue/app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
 		'hostname' => 'localhost',
 		'username' => 'root',
 		'password' => '',
-		'database' => 'hypothesis',
+		'database' => 'thinkframe',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
