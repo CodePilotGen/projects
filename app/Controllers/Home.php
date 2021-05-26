1e713ed6bac92f1835f1c3375e16e48f42592dfa
Seb <seb@mapovis.com.au>
2021-05-27 08:33:45 +1000

    ; missed.

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -39,7 +39,7 @@ class Home extends BaseController
 	}
 
 	public function loadDashBoard() {
-		return view('app/home')
+		return view('app/home');
 	}
 
 	//--------------------------------------------------------------------
